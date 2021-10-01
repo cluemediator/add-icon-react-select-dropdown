b08ed5aeabc1d255f085708d9a994c69a2de713f
Clue Mediator <cluemediator@gmail.com>
2021-10-01 10:37:14 +0530

    Implement demo

---
+++ README.md
@@ -0,0 +1,24 @@
+# add-icon-react-select-dropdown
+
+How to add an icon in the react-select dropdown
+
+## Documentation
+
+[https://www.cluemediator.com/how-to-add-an-icon-in-the-react-select-dropdown](https://www.cluemediator.com/how-to-add-an-icon-in-the-react-select-dropdown)
+
+## Quick Start
+
+Follow the below steps to run the project.
+
+1. Clone repository
+2. Run `npm i` command to install dependencies
+3. Execute `npm start` command to run the project
+
+## Connect with us
+
+Website: [Clue Mediator](https://www.cluemediator.com)  
+Like us on [Facebook](https://www.facebook.com/thecluemediator)  
+Follow us on [Twitter](https://twitter.com/cluemediator)  
+Join us on [Telegram](https://t.me/cluemediator)  
+Subscribe us on [YouTube](https://www.youtube.com/ClueMediator)  
+Follow us on [Instagram](https://www.instagram.com/clue_mediator)
---
+++ src/App.js
@@ -0,0 +1,67 @@
+import React, { useState } from 'react';
+import Select from 'react-select';
+
+function App() {
+  const data = [
+    {
+      value: 1,
+      text: 'Up Arrow',
+      icon: <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-up-circle" viewBox="0 0 16 16">
+        <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V11.5z" />
+      </svg>
+    },
+    {
+      value: 2,
+      text: 'Down Arrow',
+      icon: <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-down-circle" viewBox="0 0 16 16">
+        <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V4.5z" />
+      </svg>
+    },
+    {
+      value: 3,
+      text: 'Left Arrow',
+      icon: <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left-circle" viewBox="0 0 16 16">
+        <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z" />
+      </svg>
+    },
+    {
+      value: 4,
+      text: 'Right Arrow',
+      icon: <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-circle" viewBox="0 0 16 16">
+        <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z" />
+      </svg>
+    }
+  ];
+
+  const [selectedOption, setSelectedOption] = useState(null);
+
+  // handle onChange event of the dropdown
+  const handleChange = e => {
+    setSelectedOption(e);
+  }
+
+  return (
+    <div className="App">
+      <b>Add an icon in dropdown - <a href="https://www.cluemediator.com" target="_blank">Clue Mediator</a></b><br /><br />
+
+      <Select
+        placeholder="Select Option"
+        value={selectedOption}
+        options={data}
+        onChange={handleChange}
+        getOptionLabel={e => (
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+            {e.icon}
+            <span style={{ marginLeft: 5 }}>{e.text}</span>
+          </div>
+        )}
+      />
+
+      {selectedOption && <div style={{ marginTop: 20, lineHeight: '25px' }}>
+        <b>Selected Option:</b> {selectedOption.value}
+      </div>}
+    </div>
+  );
+}
+
+export default App;
